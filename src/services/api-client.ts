fbec5845d9f338a88416e437e75be27acdbfea01
emibonezzi <emibonezzi@gmail.com>
2024-05-16 19:44:46 -0400

    Fix: cors issues

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://30j9tnasc6.execute-api.us-east-1.amazonaws.com",
+  baseURL: "https://2f1hgmovog.execute-api.us-east-1.amazonaws.com",
 });
 
 class APIClient {
