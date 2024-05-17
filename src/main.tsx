32453c087a77e62ac6317c21eb21a8fbb1ae5ce2
emibonezzi <emibonezzi@gmail.com>
2024-05-17 19:19:11 -0400

    Force dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -9,9 +9,21 @@ import router from "./routes.tsx";
 
 const queryClient = new QueryClient();
 
+// 1. import `extendTheme` function
+import { extendTheme } from "@chakra-ui/react";
+
+// 2. Add your color mode config
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+// 3. extend the theme
+const theme = extendTheme({ config });
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
         <ReactQueryDevtools />
